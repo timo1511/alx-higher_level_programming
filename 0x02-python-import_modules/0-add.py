0aa8bdfeb4ee959ab72f419c27de253b57591109
timo1511 <tim.makori@gmail.com>
2022-06-30 13:36:27 -0700

    new changes

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,8 +3,8 @@ def add(a,b):
     return(a+b)
 
 
-if __name__ == "__main__":
-    from add_0 import add
-    a = 1
-    b = 2
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+
+from add_0 import add
+a = 1
+b = 2
+print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
