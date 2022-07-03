a267931a3480e84b86220e100e885a4ad6f691ae
timo1511 <tim.makori@gmail.com>
2022-07-02 21:25:56 -0700

    the function

--- 0x02-python-import_modules/calculator_1.py
+++ 0x02-python-import_modules/calculator_1.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def add(a, b):
-    returns (a + b)
+    return (a + b)
 
 def sub(a, b):
-    returns (a - b)
+    return (a - b)
 
 def mul(a, b):
-    returns (a * b)
+    return (a * b)
 
 def div(a, b):
-    returns int(a / b)
+    return int(a / b)
