d26856e87a14192fef342c01f0a4ae9af4a8a9ab
timo1511 <tim.makori@gmail.com>
2022-07-03 07:22:42 -0700

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,12 +6,12 @@ if __name__ == "__main__":
     argv_count = len(argv)
     index = 1
     if argv_count is 0:
-        print("{:d} arguments:".format(argv_count))
+        print("{:d} arguments.".format(argv_count))
     elif argv_count is 1:
         print("{:d} argument:".format(argv_count))
         print("{:d}: {:s}".format(index, sys.argv[1]))
     else:
-        print("{:d} arguments:".format(arg_count))
+        print("{:d} arguments:".format(argv_count))
         while index <= argv_count:
             print("{:d}: {:s}".format(index, sys.argv[index]))
             index += 1
