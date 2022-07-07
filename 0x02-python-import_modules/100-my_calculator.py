3b2982462631559353eacfa7cc682639858e3aef
timo1511 <tim.makori@gmail.com>
2022-07-06 17:34:27 -0700

    my own calculator git push

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,13 +4,13 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     argv = sys.argv[1:]
     argv_count = len(argv)
-    operators = ["+", "-", "*", "//"]
+    operators = ["+", "-", "*", "/"]
     if argv_count != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
     elif sys.argv[2] not in operators:
-        print("Unknown operator. Availabor operators: +, -, * and //")
+        print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
     else:
         a = int(sys.argv[1])
@@ -22,5 +22,5 @@ if __name__ == "__main__":
             print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
         elif sys.argv[2] == "*":
             print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-        elif sys.argv[2] == "//":
-            print("{:d} // {:d} = {:d}".format(a, b, div(a, b)))
+        elif sys.argv[2] == "/":
+            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
