55ba9b3ae62c166b4499e24e0077f50412df6f62
timo1511 <tim.makori@gmail.com>
2022-07-06 17:16:42 -0700

    my own calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
-    elif sys.argv[2] not in operators:
+    elif sys.argv[2] != operators:
         print("Unknown operator. Availabor operators: +, -, * and /")
         exit(1)
     else:
