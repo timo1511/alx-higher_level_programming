ea3054acc9b23add1b4b4e200a4d211efa954599
timo1511 <tim.makori@gmail.com>
2022-07-06 18:50:57 -0700

    magic function

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
+# encoding: utf-8
 
-
-def magic_calculation (a, b):
-    
-    print(a, b)
-
+def magic_calculation(a, b):
+    nargs = len(a, b)
+    print(nargs, (a, b))
 
 if __name__ == '__main__':
     import dis
-    dis.dis(magic_calculation)
+    dis.dis(magic_function)
