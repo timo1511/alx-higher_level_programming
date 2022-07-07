b3e922ee3ad14dc7445a009702f7affbacd4ce04
timo1511 <tim.makori@gmail.com>
2022-07-06 17:52:19 -0700

    better calculator with magic

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -2,7 +2,7 @@
 
 
 def magic_calculation(a, b):
-    from magic_calculation_102 import add, sub
+    from calculator_1 import add, sub
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
